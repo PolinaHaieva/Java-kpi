fa8f2348290a06f7e2a4182fffbc914c0bd33e51
PolinaHaieva <154555673+PolinaHaieva@users.noreply.github.com>
2023-12-28 19:42:17 +0200

    Update third.java

--- 3/third.java
+++ 3/third.java
@@ -1,14 +1,66 @@
+//C3 StringBuffer
+//C17 Задано текст та масив слів. Підрахувати у скількох реченнях зустрічається кожне слово масиву.
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class third {
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
-        StringBuffer sb = new StringBuffer(
-                """     З далекого туману, з тихих озер загірної комуни шелестить шелест:то йде Марія.
-                        Я виходжу на безгранні поля, проходжу перевали і там, де жевріють кургани, похиляюсь на самотню пустельну скелю.
-                        Я дивлюсь в даль. — Тоді дума за думою, як амазонянки,джигітують навколо мене. Тоді все пропадає...
-                        Таємні вершники летять, ритмічно похитуючись, до отрогів, і гасне день; біжить у могилах дорога, а за нею — мовчазний степ...\s
-                        Я одкидаю вії і згадую... воістину моя мати — втілений прообраз тієї надзвичайної Марії\s""");
-        String[] words = sb.toString().split("\\s+");
+        String text = "Це книга.Книга стоїть в шафі, це шафа! Ця книга дуже стара, шафа також.";
+        String[] words = { "книга", "шафа", "Це" };
+
+        Map<String, Integer> wordCounts = new HashMap<>();
+        if (text.isEmpty()) {
+            System.out.println("Текст є порожнім");
+        } else if (words.length == 0) {
+            System.out.println("Масив слів є порожнім");
+
+        } else {
+            // Розділяємо текст на речення за допомогою крапки або коми як роздільника
+            String[] sentences = text.split("[.,!?]");
+
+            for (String sentence : sentences) {
+                for (String word : words) {
+                    // Перетворюємо слово та речення в нижній регістр для регістронезалежного пошуку
+                    word = word.toLowerCase();
+                    sentence = sentence.toLowerCase();
+
+                    // Використовуємо StringBuffer для збереження речення та слова
+                    StringBuffer sentenceBuffer = new StringBuffer(sentence);
+                    StringBuffer wordBuffer = new StringBuffer(word);
+
+                    // Видаляємо з речення зайві пробіли на початку і в кінці
+
+                    // Шукаємо входження слова в речення
+                    int index = sentenceBuffer.indexOf(wordBuffer.toString());
+                    int count = 0; // Лічильник кількості входжень слова в речення
+                    while (index != -1) {
+                        int sentenceEndIndex = sentenceBuffer.indexOf(" ", index);
+                        if (sentenceEndIndex == -1) {
+                            // Якщо слово в реченні знаходиться в кінці речення
+                            sentenceEndIndex = sentenceBuffer.length();
+                        }
+                        String matchedWord = sentenceBuffer.substring(index, sentenceEndIndex);
+                        wordCounts.put(matchedWord, wordCounts.getOrDefault(matchedWord, 0) + 1);
+
+                        count++; // Збільшуємо кількість входжень слова в речення
+                        if (count >= 2) {
+                            // Якщо слово зустрілося більше ніж один раз у реченні, пропускаємо його
+                            wordCounts.put(matchedWord, wordCounts.get(matchedWord) - 1);
+                        }
+
+                        // Пересуваємо індекс для пошуку інших входжень слова в речення
+                        index = sentenceBuffer.indexOf(wordBuffer.toString(), index + 1);
+                    }
+                }
+            }
+
+            // Виводимо результати
+            for (String word : words) {
+                System.out.println("Слово '" + word + "' зустрічається в "
+                        + wordCounts.getOrDefault(word.toLowerCase(), 0) + " реченнях.");
+            }
+
+        }
     }
 }
