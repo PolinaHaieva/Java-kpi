5041d7d28639f2da984494a23b089391c7b4456d
PolinaHaieva <154555673+PolinaHaieva@users.noreply.github.com>
2024-06-11 16:44:33 +0300

    Update sixth.java

--- 6/sixth.java
+++ 6/sixth.java
@@ -1,13 +1,19 @@
+// This Java code defines classes representing various electrical appliances, an apartment with a collection of appliances,
+// and methods to perform operations such as adding appliances, calculating total power consumption, sorting appliances by power consumption,
+// printing sorted appliances, and finding appliances within a given power range.
+
 class ElectricalAppliance {
     private String name;
     private double powerConsumption;
     private boolean on = true;
 
+    // Constructor to initialize ElectricalAppliance objects
     public ElectricalAppliance(String name, double powerConsumption) {
         this.name = name;
         this.powerConsumption = powerConsumption;
     }
 
+    // Method to retrieve the power consumption of the appliance
     public double getPowerConsumption() {
         if (on)
             return powerConsumption;
@@ -15,52 +21,61 @@ class ElectricalAppliance {
             return 0;
     }
 
+    // Method to toggle the power state of the appliance
     public void turn() {
         on = !on;
     }
 
+    // Override toString() method to provide a string representation of ElectricalAppliance object
     @Override
     public String toString() {
-        return name + " (Потужність: " + getPowerConsumption() + " Вт)";
+        return name + " (Power Consumption: " + getPowerConsumption() + " W)";
     }
 }
 
+// Subclass representing a Light appliance
 class Light extends ElectricalAppliance {
     public Light(String name, double powerConsumption) {
         super(name, powerConsumption);
     }
 }
 
+// Subclass representing a Microwave appliance
 class Microwave extends ElectricalAppliance {
     public Microwave(String name, double powerConsumption) {
         super(name, powerConsumption);
     }
 }
 
+// Subclass representing a WashingMachine appliance
 class WashingMachine extends ElectricalAppliance {
     public WashingMachine(String name, double powerConsumption) {
         super(name, powerConsumption);
     }
 }
 
+// Class representing an Apartment with a collection of ElectricalAppliance objects
 class Apartment {
     private ElectricalAppliance[] appliances;
     private int numberOfAppliances;
 
+    // Constructor to initialize an Apartment object with a given capacity
     public Apartment(int capacity) {
         appliances = new ElectricalAppliance[capacity];
         numberOfAppliances = 0;
     }
 
+    // Method to add an appliance to the apartment
     public void addAppliance(ElectricalAppliance appliance) {
         if (numberOfAppliances < appliances.length) {
             appliances[numberOfAppliances] = appliance;
             numberOfAppliances++;
         } else {
-            System.out.println("Квартира переповнена. Неможливо додати більше приладів.");
+            System.out.println("The apartment is full. Unable to add more appliances.");
         }
     }
 
+    // Method to calculate the total power consumption of all appliances in the apartment
     public double calculateTotalPowerConsumption() {
         double totalPower = 0;
         for (int i = 0; i < numberOfAppliances; i++) {
@@ -69,6 +84,7 @@ class Apartment {
         return totalPower;
     }
 
+    // Method to sort the appliances in the apartment by power consumption
     public void sortByPowerConsumption() {
         for (int i = 0; i < numberOfAppliances - 1; i++) {
             for (int j = 0; j < numberOfAppliances - i - 1; j++) {
@@ -81,15 +97,17 @@ class Apartment {
         }
     }
 
+    // Method to print the sorted list of appliances by power consumption
     public void printSortedAppliances() {
-        System.out.println("Сортування за потужністю:");
+        System.out.println("Sorting by power consumption:");
         for (int i = 0; i < numberOfAppliances; i++) {
             System.out.println(appliances[i]);
         }
     }
 
+    // Method to find appliances within a given power range
     public void findAppliancesInPowerRange(double minPower, double maxPower) {
-        System.out.println("Прилади у діапазоні потужності " + minPower + " - " + maxPower + " Вт:");
+        System.out.println("Appliances in the power range " + minPower + " - " + maxPower + " W:");
         for (int i = 0; i < numberOfAppliances; i++) {
             double power = appliances[i].getPowerConsumption();
             if (power >= minPower && power <= maxPower) {
@@ -99,27 +117,19 @@ class Apartment {
     }
 }
 
+// Main class to test the functionality of the Apartment class
 public class sixth {
 
     public static void main(String[] args) {
+        // Create an Apartment object with a capacity of 6 appliances
         Apartment apartment = new Apartment(6);
 
-        apartment.addAppliance(new ElectricalAppliance("Холодильник", 150));
-        apartment.addAppliance(new Light("Люстра", 25));
-        apartment.addAppliance(new Microwave("Мікрохвильовка", 1200));
-        apartment.addAppliance(new WashingMachine("Пральна машина", 900));
-        apartment.addAppliance(new ElectricalAppliance("Телевізор", 60));
-        ElectricalAppliance teleport = new ElectricalAppliance("Телепорт", 99999);
-        // teleport.turn();
-        apartment.addAppliance(teleport);
+        // Add various appliances to the apartment
+        apartment.addAppliance(new ElectricalAppliance("Refrigerator", 150));
+        apartment.addAppliance(new Light("Chandelier", 25));
+        apartment.addAppliance(new Microwave("Microwave Oven", 1200));
+        apartment.addAppliance(new WashingMachine("Washing Machine", 900));
+        apartment.addAppliance(new ElectricalAppliance("Television", 60));
+        ElectricalAppliance teleport = new ElectricalAppliance("Teleport", 999
 
-        System.out.println("Загальна потужність: " + apartment.calculateTotalPowerConsumption() + " Вт");
-
-        apartment.sortByPowerConsumption();
-        apartment.printSortedAppliances();
-
-        double minPower = 50;
-        double maxPower = 200;
-        apartment.findAppliancesInPowerRange(minPower, maxPower);
-    }
 }
